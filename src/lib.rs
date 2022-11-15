28a1eb1c8bfa2b574704749a650c45ea78f9216b
Daniel R <nzdanron@gmail.com>
2022-11-16 12:40:26 +1300

    Added word.rs module containing the Word struct, which contains a vec of Letter structs. The Word struct will represent a word to be displayed. Written a failing test for the constructor

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod letter;
+pub mod word;
 
---
+++ src/word.rs
@@ -0,0 +1,41 @@
+use crate::letter::Letter;
+
+/// Struct to represent a wordle guess as a word
+pub struct Word {
+    letters: Vec<Letter>
+}
+
+impl Word {
+    /// Returns a Word Struct
+    ///
+    /// Parameters
+    /// word:   The word the struct represents
+    pub fn new(word: String) -> Word {
+        Word { letters: Vec::new() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// Test for checking if the build function returns a Word struct with the expected letters
+    fn new_struct_works() {
+        let word = String::from("guess");
+
+        let word_struct = Word::new(word.clone());
+
+        let mut correct_word = false;
+
+        for i in 0..word_struct.letters.len() {
+            if word_struct.letters[i].letter() != &word.chars().nth(i).unwrap() {
+                correct_word = false;
+                break;
+            }
+            correct_word = true;
+        }
+
+        assert!(correct_word);
+    }
+}
