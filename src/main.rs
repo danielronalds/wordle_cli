3f1b631e02248de76a44321320b7e2a2cfbf65b1
Daniel R <nzdanron@gmail.com>
2022-11-17 21:53:20 +1300

    Implemented reading from a word_list... again rough code but I'll clean it up later. Also written a test which sometimes fails due to the small sample size

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use std::io;
 use std::io::stdout;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+use rand::Rng;
 
 use wordle_cli;
 use wordle_cli::word::BuildErrors;
 use wordle_cli::word::Word;
 
-use crossterm::{cursor, execute, style::Print, terminal, Result};
+use crossterm::{cursor, execute, terminal, Result};
 
 fn main() -> Result<()> {
-    let word_to_guess = String::from("there");
+
+    let words_to_guess = lines_from_file("/home/danielronalds/Documents/rust/wordle_cli/wordle_words.txt")?;
+
+    let word_to_guess = random_word(words_to_guess);
+
+    println!("{}", &word_to_guess);
 
     let mut guesses: Vec<Word> = Vec::new();
 
@@ -70,4 +78,49 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+/// Reads the lines from an input file into a Vec of strings
+///
+/// Parameters:
+/// filename:   The path of the filename
+fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
+    BufReader::new(File::open(filename)?).lines().collect()
+}
+
+fn random_word(word_list: Vec<String>) -> String {
+    let random_index: usize = rand::thread_rng().gen_range(0..word_list.len());
+
+    word_list[random_index].clone()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    /// Checks if the random_word function generates two random words. This test will sometimes
+    /// fail due to the small sample size
+    fn random_word_generated() {
+        let word_list = vec![
+            String::from("three"),
+            String::from("potty"),
+            String::from("there"),
+            String::from("panic"),
+            String::from("rusty"),
+            String::from("sjkdl"),
+            String::from("fjslk"),
+            String::from("paosd"),
+            String::from("asdff"),
+            String::from("asdfa"),
+            String::from("vbsoc"),
+            String::from("adlaf"),
+            String::from("askdl"),
+            String::from("fyudi"),
+        ];
+        let word_one = random_word(word_list.clone());
+        let word_two = random_word(word_list);
+
+        assert!(!(word_one == word_two))
+    }
 }
