a8de1acc36261fc05ae455e10ce615bfae2d134c
Daniel R <nzdanron@gmail.com>
2022-11-17 19:10:07 +1300

    Formated the code and adjusted it so the winning game state is shown... it looks better?

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use std::io;
 use std::io::stdout;
 
 use wordle_cli;
-use wordle_cli::word::Word;
 use wordle_cli::word::BuildErrors;
+use wordle_cli::word::Word;
 
 use crossterm::{cursor, execute, style::Print, terminal, Result};
 
@@ -18,12 +18,15 @@ fn main() -> Result<()> {
         execute!(
             stdout(),
             terminal::Clear(terminal::ClearType::FromCursorDown)
-        ).unwrap();
+        )
+        .unwrap();
 
         wordle_cli::display_game_state(&guesses);
 
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Could not read the line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read the line");
 
         let guess = guess.trim().to_string();
 
@@ -38,25 +41,33 @@ fn main() -> Result<()> {
             Err(err) => {
                 match err {
                     BuildErrors::TooLongOfWord => println!("Words cannot be longer than 5 letters"),
-                    BuildErrors::TooShortOfWord => println!("Words cannot be shorter than 5 letters"),
-                    BuildErrors::NonAlphabeticCharcter => println!("Words can only contain alphabetic characters!"),
+                    BuildErrors::TooShortOfWord => {
+                        println!("Words cannot be shorter than 5 letters")
+                    }
+                    BuildErrors::NonAlphabeticCharcter => {
+                        println!("Words can only contain alphabetic characters!")
+                    }
                 }
 
-                io::stdin().read_line(&mut String::new()).expect("Could not read the line");
+                io::stdin()
+                    .read_line(&mut String::new())
+                    .expect("Could not read the line");
                 execute!(stdout(), cursor::MoveUp(2))?;
             }
         }
 
-
         execute!(stdout(), cursor::MoveUp(19))?;
+
+        if game_over {
+            execute!(
+                stdout(),
+                terminal::Clear(terminal::ClearType::FromCursorDown)
+            )
+            .unwrap();
+
+            wordle_cli::display_game_state(&guesses);
+        }
     }
-    let word1 = Word::new(String::from("crane"), &word_to_guess).unwrap();
-    let word2 = Word::new(String::from("horse"), &word_to_guess).unwrap();
-    let word3 = Word::new(String::from("shore"), &word_to_guess).unwrap();
-    let word4 = Word::new(String::from("there"), &word_to_guess).unwrap();
-
-    let words = vec![word1, word2, word3, word4];
-
 
     Ok(())
 }
